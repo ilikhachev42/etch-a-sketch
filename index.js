a7392c07dcc917500750229ae14035b1c09fe0f6
justkoi42 <justkoi42@gmail.com>
2023-07-18 21:05:41 +0530

    Add a function for resizing grid.

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 const grid = document.getElementById('grid');
 grid.innerHTML = '<div class="pixel"></div>'.repeat(256);
-const pixel = document.querySelectorAll('.pixel');
+const pixel = document.getElementsByClassName('pixel');
+console.log(pixel)
 
 const setColor = function(event) {
 	event.target.setAttribute('class', 'pixel new');
 }
 
-const hoverMode = function(setColor) {
-	for (let i = 0; i < pixel.length; i++) {
-		if (event.target !== grid) {
-			pixel[i].setAttribute('onmouseover', 'setColor(event)');
-		}
-	}
+let isHoverOn = false;
+
+function hoverMode(event) {
+		isHoverOn = !isHoverOn;
+
+    if (isHoverOn) {
+      for (let i = 0; i < pixel.length; i++) {
+        if (event.target !== grid) {
+          pixel[i].addEventListener('mouseover', setColor)
+        }
+      }
+    }
+
+    if (!isHoverOn) {
+      for (let i = 0; i < pixel.length; i++) {
+        if(event.target !== grid) {
+          pixel[i].removeEventListener('mouseover', setColor)
+        }
+      }
+    }
 }
 
 grid.addEventListener('click', hoverMode);
+
+//Resizing grid
+
+let buttonValue = 0;
+
+const addDiv = function(num) {
+	grid.innerHTML = '<div class="pixel"></div>'.repeat(num);
+}
+
+const setGridSize = function(event) {
+	if (event.target.hasAttribute('value')) {
+		buttonValue = event.target.value;
+		addDiv(buttonValue * buttonValue);
+	}
+}
+
+addEventListener('click', setGridSize);
